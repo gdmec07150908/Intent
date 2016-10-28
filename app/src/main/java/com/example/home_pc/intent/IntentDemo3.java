511dc1fd9d00c26d870642c7b1bfe9a465a9b6e0
gdmec07150908 <942944702@qq.com>
2016-10-28 12:07:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/home_pc/intent/IntentDemo3.java
@@ -0,0 +1,13 @@
+package com.example.home_pc.intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntentDemo3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
